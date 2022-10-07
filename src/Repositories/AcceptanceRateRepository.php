b12e7074c3afb0c904db25328f3d43e42469cf2e
Jay Slater <jay.slater@imegamedia.co.uk>
2022-10-07 13:40:19 +0100

    Add filter to get acceptance rates by clientId

--- src/Repositories/AcceptanceRateRepository.php
+++ src/Repositories/AcceptanceRateRepository.php
@@ -13,23 +13,29 @@ final class AcceptanceRateRepository
      *
      * @param CarbonInterface $startDate
      * @param CarbonInterface $endDate
+     * @param int|null $clientId
      * @return Collection
      */
     public function getAcceptanceRateByDate
     (
-        CarbonInterface $startDate,
-        CarbonInterface $endDate,
+        string $startDate,
+        string $endDate,
+        ?int $clientId = null,
     ): Collection
     {
-        return AcceptanceRate::query()
+        $qb =  AcceptanceRate::query()
             ->select([
                 'finance_providers.id AS finance_providers_id',
                 'finance_providers.alias AS finance_providers_alias',
             ])
             ->join('finance_providers', 'finance_provider_id', 'finance_providers.id')
             ->selectRaw('AVG(acceptance_rate) as average_acceptance_rate')
-            ->whereBetween('sampled_at', [$startDate, $endDate])
-            ->groupBy('finance_provider_id', 'finance_providers_id', 'finance_providers_alias')
-            ->get();
+            ->whereBetween('sampled_at', [$startDate, $endDate]);
+
+        if ($clientId) {
+            $qb->where('client_id', $clientId);
+        }
+
+        return $qb->groupBy('finance_provider_id', 'finance_providers_id', 'finance_providers_alias')->get();
     }
 }
